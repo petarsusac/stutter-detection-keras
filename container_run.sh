cf02de191a0cd654d415ac34253c962599e57557
Petar Susac <petar.susac@fer.hr>
2024-03-23 16:58:56 +0100

    Change job name and resource requirements

--- container_run.sh
+++ container_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#PBS -N apptainer-test
+#PBS -N stutter-detection-keras
 #PBS -o out/out.txt
 #PBS -e out/err.txt
+#PBS -l select=1:ncpus=1:ngpus=1
 cd $PBS_O_WORKDIR
 apptainer run --nv container.sif
