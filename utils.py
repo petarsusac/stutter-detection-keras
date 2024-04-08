4fafd5624e8f9f29c66283c8e2b4d21c219c3823
Petar Susac <petar.susac@fer.hr>
2024-04-08 10:54:17 +0200

    Add dataset resampling util functions

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sklearn
+import pandas as pd
 
 def show_label_distribution(labels: dict) -> None:
     print('Total samples:', len(next(iter(labels.values()))))
@@ -18,3 +20,19 @@ def show_label_distribution_single(labels: np.array) -> None:
 def confusion_matrix(y_true, y_pred, th=0.5):
     y_pred = (y_pred > th).astype(np.float32)
     return sklearn.metrics.confusion_matrix(y_true, y_pred)
+
+def resample_negatives(df: pd.DataFrame, column_name: str, th, random_state: int):
+    df_negatives = df[df[column_name] < th]
+    df_positives = df[df[column_name] >= th]
+
+    df_negatives = df_negatives.sample(len(df_positives), random_state=random_state)
+    
+    return pd.concat([df_negatives, df_positives]).sample(frac=1, random_state=random_state)
+
+def resample_positives(df: pd.DataFrame, column_name: str, th, random_state: int):
+    df_negatives = df[df[column_name] < th]
+    df_positives = df[df[column_name] >= th]
+
+    df_positives = df_positives.sample(len(df_negatives), random_state=random_state, replace=True)
+
+    return pd.concat([df_negatives, df_positives]).sample(frac=1, random_state=random_state)
