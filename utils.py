443f514facf29e9d03bd0e34a200902b9f4acf2a
Petar Susac <petar.susac@fer.hr>
2024-04-11 12:15:03 +0200

    Add augmented upsampling

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def confusion_matrix(y_true, y_pred, th=0.5):
     y_pred = (y_pred > th).astype(np.float32)
     return sklearn.metrics.confusion_matrix(y_true, y_pred)
 
-def resample_negatives(df: pd.DataFrame, column_name: str, th, random_state: int):
+def resample_negatives(df: pd.DataFrame, column_name: str, th: int, random_state: int):
     df_negatives = df[df[column_name] < th]
     df_positives = df[df[column_name] >= th]
 
@@ -29,10 +29,31 @@ def resample_negatives(df: pd.DataFrame, column_name: str, th, random_state: int
     
     return pd.concat([df_negatives, df_positives]).sample(frac=1, random_state=random_state)
 
-def resample_positives(df: pd.DataFrame, column_name: str, th, random_state: int):
+def resample_positives(df: pd.DataFrame, column_name: str, th: int, random_state: int):
     df_negatives = df[df[column_name] < th]
     df_positives = df[df[column_name] >= th]
 
     df_positives = df_positives.sample(len(df_negatives), random_state=random_state, replace=True)
 
     return pd.concat([df_negatives, df_positives]).sample(frac=1, random_state=random_state)
+
+def resample_positives_augmentation(df: pd.DataFrame, column_name: str, th: int, random_state: int):
+    df_positives_original = df[df[column_name] >= th]
+    df_negatives = df[df[column_name] < th]
+
+    df_positives_resampled = df_positives_original.sample(len(df_negatives) - len(df_positives_original), random_state=random_state, replace=True)
+    df_positives_resampled['Augment'] = True
+
+    return pd.concat([df_negatives, df_positives_original, df_positives_resampled]).sample(frac=1, random_state=random_state)
+
+def resample_positives_augmented_multilabel(df: pd.DataFrame, column_names: list, th: int, random_state: int):
+    for name in column_names:
+        df_positives_original = df[df[name] >= th]
+        df_negatives = df[df[name] < th]
+
+        df_positives_resampled = df_positives_original.sample(len(df_negatives) - len(df_positives_original), random_state=random_state, replace=True)
+        df_positives_resampled['Augment'] = True
+
+        df = pd.concat([df_negatives, df_positives_original, df_positives_resampled]).sample(frac=1, random_state=random_state)
+    
+    return df
