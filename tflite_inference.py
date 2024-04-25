27ea5b6d205fbee7114d0c024214e00be6c95f31
Petar Susac <petar.susac@fer.hr>
2024-04-25 17:19:12 +0200

    Modify TF lite inferencing

--- tflite_inference.py
+++ tflite_inference.py
@@ -12,13 +12,13 @@ TFLITE_MODEL_PATH = 'trained_models/tflite/block.tflite'
 TF_MODEL_PATH = 'trained_models/keras/block'
 LABEL = 'Block'
 
-X_train, Y_train, X_test, Y_test = get_dataset([LABEL])
+X_train, Y_train, X_test, Y_test = get_dataset([LABEL], True)
 
-# sample = X_test[500]
-# sample_label = Y_test[LABEL][500]
+sample = X_test[23]
+sample_label = Y_test[LABEL][23]
 
-sample = FeatureExtractor.mfcc('silence.wav', n_fft=2048, hop=512, normalize=False, transpose=False)
-sample_label = 0
+# sample = FeatureExtractor.mfcc('silence.wav', n_fft=2048, hop=512, normalize=False, transpose=False)
+# sample_label = 0
 
 sample = (sample - np.mean(X_train)) / np.std(X_train)
 
@@ -28,18 +28,18 @@ output_tf = Model.predict_single(tf_model, sample)[LABEL].numpy()[0][0]
 interpreter = tf.lite.Interpreter(model_path=TFLITE_MODEL_PATH)
 interpreter.allocate_tensors()
 
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-input_scale = input_details[0]['quantization'][0]
-input_zero_point = input_details[0]['quantization'][1]
-output_scale = output_details[0]['quantization'][0]
-output_zero_point = output_details[0]['quantization'][1]
+input_details = interpreter.get_input_details()[0]
+output_details = interpreter.get_output_details()[0]
+input_scale = input_details['quantization'][0]
+input_zero_point = input_details['quantization'][1]
+output_scale = output_details['quantization'][0]
+output_zero_point = output_details['quantization'][1]
 
-interpreter.set_tensor(input_details[0]['index'], np.ndarray.astype(np.array([sample / input_scale + input_zero_point]), np.int8))
+interpreter.set_tensor(input_details['index'], np.ndarray.astype(np.expand_dims(sample / input_scale + input_zero_point, axis=0), np.int8))
 
 interpreter.invoke()
 
-output_tensor = interpreter.get_tensor(output_details[0]['index'])
+output_tensor = interpreter.get_tensor(output_details['index'])
 output_tflite = (output_tensor[0][0] - output_zero_point) * output_scale
 
 print('True label:', sample_label)
