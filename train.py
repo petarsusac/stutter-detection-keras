9b2d831f15c330983c992e57ab1c700e2dd70be0
Petar Susac <petar.susac@fer.hr>
2024-04-25 17:19:38 +0200

    Remove early stopping callback

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import datetime
 
 pos_labels = ['Block']
 
-X_train, Y_train, X_test, Y_test = get_dataset(pos_labels)
+X_train, Y_train, X_test, Y_test = get_dataset(pos_labels, True)
 
 # Build the model
 model = ConvLSTM.create_model(pos_labels, input_shape=(X_train.shape[1], X_train.shape[2]))
@@ -20,11 +20,11 @@ callbacks = [
         log_dir = 'logs/fit/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"),
         histogram_freq = 1,
     ),
-    keras.callbacks.EarlyStopping(
-        monitor='val_loss',
-        patience=10,
-        restore_best_weights=True
-    )
+    # keras.callbacks.EarlyStopping(
+    #     monitor='val_loss',
+    #     patience=10,
+    #     restore_best_weights=True
+    # )
 ]
 
 model.fit(X_train, Y_train, 
