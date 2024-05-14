85191856e3d6668a7e872a1dd4378652f6e76a3c
Petar Susac <petar.susac@fer.hr>
2024-05-14 12:24:14 +0200

    Normalize confusion matrix

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def confusion_matrix(y_true, y_pred, th=0.5):
     y_pred = (y_pred > th).astype(np.float32)
-    return sklearn.metrics.confusion_matrix(y_true, y_pred)
+    return sklearn.metrics.confusion_matrix(y_true, y_pred, normalize='true')
 
 def save_model(model, path: str, input_shape: tuple):
     run_model = tf.function(lambda x: model(x))
